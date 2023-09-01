cadabca64a1ad44c6211f0dc22137d76be466aa7
Saran <113179649+Santosaran@users.noreply.github.com>
2023-09-01 23:29:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 function App() {
   const [data, setData] = useState({})
   const [location, setLocation] = useState('')
-
+ 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`
 
   const searchLocation = (event) => {
